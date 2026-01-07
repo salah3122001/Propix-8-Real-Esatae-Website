769d75f799c6e4a4abca092eec9ce5f51358167b
salah312201 <mohamed_ashraf4444@hotmail.com>
2026-01-07 15:06:59 +0200

    update htaccess

--- config/filesystems.php
+++ config/filesystems.php
@@ -40,7 +40,7 @@ return [
 
         'public' => [
             'driver' => 'local',
-            'root' => storage_path('app/public'),
+            'root' => $storagePath = storage_path('app/public'),
             'url' => env('FILESYSTEM_PUBLIC_URL', env('APP_URL') . '/storage'),
             'visibility' => 'public',
             'throw' => false,
