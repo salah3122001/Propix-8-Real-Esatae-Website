9cd346bed3d59ccba9d6ff9943bae85d42f8abf2
Mohamed Ashraf <mohamed_ashraf4444@hotmail.com>
2026-01-25 17:01:58 +0200

    add frontend files

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return ['Laravel' => app()->version()];
-});
+// Root route is handled by frontend (see .htaccess)
+// Route::get('/', function () {
+//     return ['Laravel' => app()->version()];
+// });
 
 require __DIR__ . '/auth.php';
